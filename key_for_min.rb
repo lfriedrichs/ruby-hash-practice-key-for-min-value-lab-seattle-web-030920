63ab2aa1a03d5cae2e63ec1559a764d16816a3f3
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-30 14:41:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  name_hash.min.key
+def key_for_min_value(hash)
+  hash.min.first
 end
