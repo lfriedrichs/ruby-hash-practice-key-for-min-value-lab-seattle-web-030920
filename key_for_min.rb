79ecb7bd8bdb0b6a3ea906d6cf4be428960785ec
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-30 14:42:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  hash.min.first
+  output = hash.min
+  if output 
+    output.first 
+  end
 end
